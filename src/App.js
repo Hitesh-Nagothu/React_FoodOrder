590a9317237fe4db7cd6d64541ae756dc0ecf389
Hitesh <hiteshnagothu@gmail.com>
2021-04-24 19:01:56 -0400

    Added Cart Modal popup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 function App() {
+
+  const [isOpen, setisOpen] = useState(false)
+
+  const setOpen = () => {
+    setisOpen(true)
+  };
+
+  const setClose= () => {
+    setisOpen(false)
+  }
+
   return (
     <Fragment>
-      <Header />
+      {isOpen && <Cart setClose={setClose}  />}
+      <Header setOpen={setOpen} />
       <main>
         <Meals />
       </main>
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton setOpen={props.setOpen} setClose={props.setClose} />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt='A table full of delicious food!' />
